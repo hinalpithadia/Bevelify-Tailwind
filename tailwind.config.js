4e5a90ff4decdaac280595f40a3e7756b5da1934
hinalpithadia <38879377+hinalpithadia@users.noreply.github.com>
2025-03-27 15:17:56 +0530

    css changes added and new faq page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ module.exports = {
         'darkblue-hover': '#283585',
         'gold': '#DBB079'
       },
+      backgroundImage: {
+        'custom-gradient-bronze': 'linear-gradient(263deg, #DC8F51 80.95%, #FFDBBD 75.91%, #DC8F51 99%)',
+        'custom-gradient-silver': 'linear-gradient(263deg, #AEBCBB 80.95%, #FFF 93.91%, #AEBCBB 74.61%)',
+        'custom-gradient-gold': 'linear-gradient(263deg, #F99D1A  80.95%, #FFD79D 93.91%, #F99D1A 74.61%)',
+      },
       scrollbar: {
         hide: {
           'scrollbar-width': 'none', /* For Firefox */
